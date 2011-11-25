cd71c6a52b7d1f2374921b605a7128bdc3e1fb20
Jovani Favero <jovani.favero@gmail.com>
2011-11-25 20:22:39 -0200

    My second commit 25/11/2011

---
+++ src/main.C
@@ -0,0 +1,16 @@
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+
+int main()
+{
+
+    unsigned dim, nMom, nEq, nPoints, nVar, overEq;
+
+
+    cout << "\n\n";
+ 
+    return 0;
+}
